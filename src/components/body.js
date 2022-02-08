5ec85f31914d6aafd252fa297a5eba7cf97e8e6a
djibadiawara <djiba.diawara97@gmail.com>
2022-02-08 13:58:01 -0500

    State to body

--- src/components/body.js
+++ src/components/body.js
@@ -1,12 +1,21 @@
 import DataContainer from './dataContainer';
 import Sidebar from './sidebar';
+import {useState, useEffect} from 'react'
+
 
 function Body(){
+    const [destinations, setDestinations] = useState([])
+
+    useEffect (()=>{
+        fetch('http://localhost:3000/destinations')
+       .then(r => r.json())
+        .then(destinations => setDestinations(destinations) )
+      },[])
     return(
         <div className="body">
             <Sidebar className="body-element" />
             <hr/>
-            <DataContainer className="body-element" />
+            <DataContainer className="body-element" destinations={destinations} />
         </div>
     )
 }
