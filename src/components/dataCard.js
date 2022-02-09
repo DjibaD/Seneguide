1bb589849ee40857483337c5749921b1736f9ec8
djibadiawara <djiba.diawara97@gmail.com>
2022-02-09 16:46:34 -0500

    Description button and carousel

--- src/components/dataCard.js
+++ src/components/dataCard.js
@@ -1,13 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import {Card} from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
+import {useState} from 'react'
 function DataCard({destination}) {
-    // console.log(destination)
+    const [isShort, setIsShort]= useState(true)
+
+    function handleClick (){
+        setIsShort(!isShort)
+    }
     return (
         <Card >
             <Card.Img src={destination.image}/>
             <Card.Title><h2>{destination.name}</h2></Card.Title>
             <p>{destination.location}</p>
-            <p>{destination.description}</p>
+            <p>
+                {isShort? destination.description.slice(0,150) : destination.description }
+
+
+            </p>  
+
+            <Button onClick={handleClick}> Read more </Button>
            
 
            </Card>
--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,57 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import {Navbar, NavbarBrand,Container,Row,Col} from 'react-bootstrap'
-
+import image1 from './../assets/Pink Lake.jpg'
+import image2 from './../assets/St Louis City.jpg'
+import {Navbar, NavbarBrand,Container,Row,Col, Nav,Carousel} from 'react-bootstrap'
 function Header() {
     const logo = "https://upload.wikimedia.org/wikipedia/commons/f/fd/Flag_of_Senegal.svg"
     return (
         <>
+         <Navbar className="justify-content-center">
+            <Navbar.Brand>Seneguide</Navbar.Brand>
+            <Nav>
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">About</Nav.Link>
+                 <Nav.Link href="#pricing"></Nav.Link>
+          </Nav>
+        </Navbar>
+      <Carousel>
+      <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={image1}
+      alt="Second slide"
+    />
+     </Carousel.Item>
+     <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={image2}
+      alt="Second slide"
+    />
+     </Carousel.Item>
+      </Carousel>
+
+
         <Container>
+
             <Row >
                 <Col></Col>
                 <Col>
         <Navbar bg="mywhite" variant="light">
-            <Navbar.Brand>
-                        <img src={logo}/>
-            </Navbar.Brand> 
-            <Navbar.Brand></Navbar.Brand>
-            <Navbar.Brand>
-            <h1>Things To Do In Senegal</h1> 
-            </Navbar.Brand>
+        <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src="https://upload.wikimedia.org/wikipedia/commons/f/fd/Flag_of_Senegal.svg"
+          width="40"
+          height="65"
+          className="d-inline-block align-top"
+        />{' '}
 
+      </Navbar.Brand>
+            <Navbar.Brand>
+            <h1>Places To Visit In Senegal</h1> 
+            </Navbar.Brand> 
+            
             </Navbar>
             </Col>
             <Col></Col>
