1aae9ca3cced229603169404c15c3984df6ee58a
djibadiawara <djiba.diawara97@gmail.com>
2022-02-11 15:48:35 -0500

    form

--- src/components/form.js
+++ src/components/form.js
@@ -1,8 +1,58 @@
-
+import { useState } from 'react'
 
 function Form() {
+    const [destinationForm, setDestinationForm] = useState({
+        name: "",
+        image: "",
+        location: "",
+        description: ""
+    })
+
+    function manageDestinationForm(event) {
+        console.log(event.target.value)
+        let name = event.target.name;
+        let value = event.target.value;
+        setDestinationForm({
+            ...destinationForm,
+            [name]: value
+        });
+    }
+
     return (
-        <div>
+        <div className="new-form">
+            <h2>Add New Destination</h2>
+            {/* onSubmit={handleSubmit} */}
+            <form >
+                <input
+                    type="text"
+                    placeholder="destination name"
+                    name="name"
+                    value={destinationForm.name}
+                    onChange={manageDestinationForm}
+                />
+                <input
+                    type="text"
+                    placeholder="Image URL"
+                    name="image"
+                    value={destinationForm.image}
+                    onChange={manageDestinationForm}
+                />
+                <input
+                    type="text"
+                    placeholder="location"
+                    name="location"
+                    value={destinationForm.location}
+                    onChange={manageDestinationForm}
+                />
+                <input
+                    type="text"
+                    placeholder="description"
+                    name="description"
+                    value={destinationForm.description}
+                    onChange={manageDestinationForm}
+                />
+                <button type="submit">Add Destination</button>
+            </form>
 
         </div>
     )
